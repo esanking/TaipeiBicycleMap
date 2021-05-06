7d600704e249134a2b8deb6603565a5e3b59c878
esanking <asd500373@gmail.com>
2021-05-06 12:05:13 +0800

    resolve cors

--- src/view/Map.vue
+++ src/view/Map.vue
@@ -128,8 +128,8 @@ export default {
     },
   },
   mounted() {
-    const cors = 'https://cors-anywhere.herokuapp.com/';
-    const url = 'https://data.ntpc.gov.tw/api/datasets/71CD1490-A2DF-4198-BEF1-318479775E8A/json/preview';
+    const cors = 'https://all-the-cors.herokuapp.com/';
+    const url = 'https://data.ntpc.gov.tw/api/datasets/71CD1490-A2DF-4198-BEF1-318479775E8A/json?page=0&size=1000';
     this.$http.get(`${cors}${url}`)
       .then((res) => {
         this.data = res.data;
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  publicPath: './',
+  publicPath: "/",
+  // outputDir: "public/live",
+  devServer: { //自动打开浏览器
+    port: "8080",
+    host: "localhost",
+    open: true,
+    proxy: {
+      "/api": {
+        target: "https://data.ntpc.gov.tw/api/datasets/71CD1490-A2DF-4198-BEF1-318479775E8A/", //要跨域的域名 目标地址
+        changeOrigin: true, //是否开启跨域  是否更改源路径
+        ws: true,
+        pathRewrite: {
+          "^/api": ""  // /api/ / 凡是/api开头的地址都可以跨域
+        }
+      }
+    }
+  }
 };
